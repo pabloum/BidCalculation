6cf2a1002f17681d60b8c3dd83dde36ea1594183
Pablo Uribe <pablo.um00@gmail.com>
2023-11-30 09:13:35 -0500

    Refactor calculator

--- BidCalculation.Business/PriceCalculator/FeeCalulator.cs
+++ BidCalculation.Business/PriceCalculator/FeeCalulator.cs
@@ -11,26 +11,72 @@ public interface IFeeCalculator
 
 public class FeeCalculator : IFeeCalculator
 {
-    public FeeCalculator()
+    private readonly IDictionary<FeeType, Func<VehicleType, int, double>> _feeCostCalculator = new Dictionary<FeeType, Func<VehicleType, int, double>>
     {
-        
-    }
+        {FeeType.BASIC, GetBasicFee},
+        {FeeType.SPECIAL, GetSpecialFee},
+        {FeeType.ASSOCIATION,  (vehicleType, basePrice) => GetAssociationFee(basePrice)},
+        {FeeType.STORAGE, (vehicleType, basePrice)  => GetStorageFee()},
+    };
 
     public IEnumerable<Fee> GetFees(int basePrice, VehicleType vehicleType)
     {
         var fees = new ConcurrentBag<Fee>();
 
-        Parallel.Invoke(
-            () => fees.Add(GetBasicFee(vehicleType, basePrice)),
-            () => fees.Add(GetSpecialFee(vehicleType, basePrice)),
-            () => fees.Add(GetAssociationFee(basePrice)),
-            () => fees.Add(GetStorageFee())
-        );
+        Parallel.ForEach(Enum.GetValues<FeeType>(), feeType => 
+        {
+            fees.Add(CreateFee(feeType, _feeCostCalculator[feeType].Invoke(vehicleType, basePrice)));
+        });
 
         return fees;
     }
 
-    private double CheckRange(double min, double max, ref double cost)
+    private Fee CreateFee(FeeType feeType, double cost)
+    {
+        return new Fee 
+        {
+            Type = feeType,
+            Cost = cost
+        };
+    }
+
+    private static double GetBasicFee(VehicleType vehicleType, int basePrice)
+    {
+        var cost = basePrice * 0.1;
+        return Math.Round(vehicleType == VehicleType.COMMON ? CheckRange(10, 50, ref cost) : CheckRange(25, 200, ref cost),2);
+    }
+
+    private static double GetSpecialFee(VehicleType vehicleType, int basePrice)
+    {
+        return Math.Round(vehicleType == VehicleType.COMMON ? basePrice * 0.02 : basePrice * 0.04, 2);
+    }
+
+    private static double GetAssociationFee(int basePrice)
+    {
+        if (basePrice == 1 || basePrice <= 500)
+        {
+            return 5.00;
+        }
+        else if (basePrice <= 1000)
+        {
+            return 10.00;
+        }
+        else if (basePrice <= 3000)
+        {
+            return 15.00;
+        }
+        else
+        {
+            return 20.00;
+        }
+    }
+
+    private static double GetStorageFee()
+    {
+        return 100.00; // Fixed fee
+    }
+
+    private static double CheckRange(double min, double max, ref double cost)
     {
         if (cost < min)
         {
@@ -44,62 +90,5 @@ public class FeeCalculator : IFeeCalculator
         {
             return cost;
         }
-    }
-
-    private Fee GetBasicFee(VehicleType vehicleType, int basePrice)
-    {
-        var cost = basePrice * 0.1;
-
-        return new Fee
-        {
-            Type = FeeType.BASIC,
-            Cost = Math.Round(vehicleType == VehicleType.COMMON ? CheckRange(10, 50, ref cost) : CheckRange(25, 200, ref cost),2)
-        };
-    }
-
-    private Fee GetSpecialFee(VehicleType vehicleType, int basePrice)
-    {
-        return new Fee
-        {
-            Type = FeeType.SPECIAL,
-            Cost = Math.Round(vehicleType == VehicleType.COMMON ? basePrice * 0.02 : basePrice * 0.04, 2)
-        };
-    }
-
-    private Fee GetAssociationFee(int basePrice)
-    {
-        var cost = 0;
-
-        if (basePrice == 1 || basePrice <= 500)
-        {
-            cost = 5;
-        }
-        else if (basePrice <= 1000)
-        {
-            cost = 10;
-        }
-        else if (basePrice <= 3000)
-        {
-            cost = 15;
-        }
-        else
-        {
-            cost = 20;
-        }
-
-        return new Fee
-        {
-            Type = FeeType.ASSOCIATION,
-            Cost = cost
-        };
-    }
-
-    private Fee GetStorageFee()
-    {
-        return new Fee
-        {
-            Type = FeeType.STORAGE,
-            Cost = 100 // Fixed fee
-        };
     }
 }
